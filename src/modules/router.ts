c6e61b28d85c64ae9c5bbb749584ccbb882c09b9
xiliangzhou <951049764@qq.com>
2022-03-18 22:08:40 +0800

    router

--- src/modules/router.ts
+++ src/modules/router.ts
@@ -4,10 +4,13 @@ import { setupLayouts } from 'virtual:generated-layouts'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = setupLayouts(generatedRoutes)
+console.log(import.meta.env)
 
 export const router = createRouter({
   routes,
-  history: createWebHistory(),
+  // 解决 二级路径存在时，路径地址路由不匹配的问题
+  // https://juejin.cn/post/7051826951463370760#heading-27
+  history: createWebHistory(import.meta.env.BASE_URL),
 })
 
 export default (app: App) => app.use(router)
