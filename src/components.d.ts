3576f9e7400287bdee0b85661ab0364a03d6201b
zhouxiliang <xiliang.zhou@edsz9.com>
2022-03-24 17:45:47 +0800

    chore: add antdv

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,12 +4,17 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
+    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
     AButton: typeof import('ant-design-vue/es')['Button']
     ALayout: typeof import('ant-design-vue/es')['Layout']
     ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
     ALayoutFooter: typeof import('ant-design-vue/es')['LayoutFooter']
     ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
     ALayoutSider: typeof import('ant-design-vue/es')['LayoutSider']
+    AMenu: typeof import('ant-design-vue/es')['Menu']
+    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
+    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
     Footer: typeof import('./components/Footer.vue')['default']
   }
 }
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,30 +1,100 @@
-<script setup lang="ts"></script>
-
 <template>
-  <a-layout>
-    <a-layout-sider>Sider</a-layout-sider>
-    <a-layout>
-      <a-layout-header>Header</a-layout-header>
-      <a-layout-content>Content</a-layout-content>
-      <a-layout-footer>Footer</a-layout-footer>
+  <div>
+    <a-layout class="components-layout-demo">
+      <a-layout-header class="header">
+        <div class="layout-logo-box">
+          <div class="layout-logo">
+            <img :src="avatar" class="logo" alt="logo" />
+            <div class="title">vite+ts+vue3</div>
+          </div>
+        </div>
+        <div class="sys-setting">aafaf</div>
+      </a-layout-header>
+      <a-layout>
+        <a-layout-sider width="200" style="background: #fff">
+          <a-menu
+            v-model:selectedKeys="selectedKeys2"
+            v-model:openKeys="openKeys"
+            mode="inline"
+            :style="{ height: '100%', borderRight: 0 }"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
+                <span>
+                  <user-outlined />
+                  subnav 1
+                </span>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
+                <span>
+                  <laptop-outlined />
+                  subnav 2
+                </span>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout style="padding: 0 24px 24px">
+          <a-breadcrumb style="margin: 16px 0">
+            <a-breadcrumb-item>Home</a-breadcrumb-item>
+            <a-breadcrumb-item>List</a-breadcrumb-item>
+            <a-breadcrumb-item>App</a-breadcrumb-item>
+          </a-breadcrumb>
+          <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+          >
+            Content
+          </a-layout-content>
+        </a-layout>
+      </a-layout>
     </a-layout>
-  </a-layout>
+  </div>
 </template>
+<script setup lang="ts">
+import { UserOutlined, LaptopOutlined } from '@ant-design/icons-vue'
+import avatar from '~/assets/avatar.png'
 
-<style scoped>
-/* a {
-  color: #42b983;
+const selectedKeys2 = ref<string[]>(['1'])
+const openKeys = ref<string[]>(['sub1'])
+</script>
+<style scoped lang="less">
+.components-layout-demo {
+  .layout-logo-box {
+    height: 100%;
+    .layout-logo {
+      display: flex;
+      .logo {
+        width: 50px;
+        height: 50px;
+      }
+      .title {
+        font-size: 20px;
+        color: black;
+        padding: 0 10px;
+      }
+    }
+  }
+  .ant-layout-header {
+    display: flex;
+    justify-content: space-between;
+    color: #fff;
+    background: #fff;
+    align-items: center;
+    padding: 0 16px;
+  }
 }
 
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
+.site-layout-background {
+  background: #fff;
 }
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
-} */
 </style>
