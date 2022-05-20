fcfb7ee9581770c9bda22d8f5f807710d806e2b6
zhouxiliang <xiliang.zhou@edsz9.com>
2022-05-20 16:56:16 +0800

    feat:autocrlf

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,15 +2,15 @@ module.exports = {
   tabWidth: 2,
   jsxSingleQuote: true,
   printWidth: 100,
-  singleQuote: true,
-  semi: false,
-  overrides: [
-    {
-      files: '*.json',
-      options: {
-        printWidth: 200,
-      },
-    },
-  ],
+  singleQuote: true, //单引号
+  semi: false, //是否在行尾加分号
+  // overrides: [
+  //   {
+  //     files: '*.json',
+  //     options: {
+  //       printWidth: 200,
+  //     },
+  //   },
+  // ],
   arrowParens: 'always',
 }
--- src/components.d.ts
+++ src/components.d.ts
@@ -4,19 +4,9 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
-    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
-    AButton: typeof import('ant-design-vue/es')['Button']
-    ALayout: typeof import('ant-design-vue/es')['Layout']
-    ALayoutContent: typeof import('ant-design-vue/es')['LayoutContent']
-    ALayoutHeader: typeof import('ant-design-vue/es')['LayoutHeader']
-    ALayoutSider: typeof import('ant-design-vue/es')['LayoutSider']
-    AMenu: typeof import('ant-design-vue/es')['Menu']
-    AMenuItem: typeof import('ant-design-vue/es')['MenuItem']
-    ASubMenu: typeof import('ant-design-vue/es')['SubMenu']
     Footer: typeof import('./components/Footer.vue')['default']
     Icon: typeof import('./components/Icon/index.tsx')['default']
   }
 }
 
-export { }
+export {}
